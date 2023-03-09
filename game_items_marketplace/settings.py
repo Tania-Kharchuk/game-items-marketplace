209e167f2d3368a76fc6c3831c1f46677441558d
Tania Kharchuk <tkharchuk14@gmail.com>
2023-03-09 23:19:34 +0200

    Add AUTH_USER_MODEL and LOGIN_REDIRECT_URL to settings.py

--- game_items_marketplace/settings.py
+++ game_items_marketplace/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'wow',
 ]
 
 MIDDLEWARE = [
@@ -100,6 +101,9 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+AUTH_USER_MODEL = 'wow.Gamer'
+
+LOGIN_REDIRECT_URL = '/'
 
 # Internationalization
 # https://docs.djangoproject.com/en/4.1/topics/i18n/
