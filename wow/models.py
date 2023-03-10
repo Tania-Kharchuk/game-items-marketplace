a17ed9e6160dde3b7746623d45c73c41d8b38638
Tania Kharchuk <tkharchuk14@gmail.com>
2023-03-10 21:31:29 +0200

    Add related_name to owner in Item model

--- wow/models.py
+++ wow/models.py
@@ -52,7 +52,7 @@ class InteractionType(models.Model):
 class Item(models.Model):
     name = models.CharField(max_length=255)
     description = models.TextField()
-    owner = models.ForeignKey(Gamer, on_delete=models.CASCADE)
+    owner = models.ForeignKey(Gamer, on_delete=models.CASCADE, related_name="items")
     price = models.DecimalField(max_digits=4, decimal_places=2)
     type = models.ForeignKey(ItemType, on_delete=models.CASCADE)
     interaction_type = models.ForeignKey(
