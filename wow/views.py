2eb27761294dc8843fba2e158b429294ff2ddf86
Tania Kharchuk <tkharchuk14@gmail.com>
2023-03-10 13:47:28 +0200

    Add index to views.py

--- wow/views.py
+++ wow/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
 
-# Create your views here.
+from wow.models import Gamer, Item, ItemType
+
+
+def index(request):
+    """View function for the home page of the site."""
+
+    num_gamers = Gamer.objects.count()
+    num_items = Item.objects.count()
+    num_item_types = ItemType.objects.count()
+
+    num_visits = request.session.get("num_visits", 0)
+    request.session["num_visits"] = num_visits + 1
+
+    context = {
+        "num_gamers": num_gamers,
+        "num_items": num_items,
+        "num_item_types": num_item_types,
+        "num_visits": num_visits + 1,
+    }
+
+    return render(request, "wow/index.html", context=context)
