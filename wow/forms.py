9eb6e6adcbee53b4b8afe625f9ce2262bb1691c6
Tania Kharchuk <tkharchuk14@gmail.com>
2023-03-12 16:37:16 +0200

    Add form for gamer playable race or class update

--- wow/forms.py
+++ wow/forms.py
@@ -1,6 +1,7 @@
 from django.contrib.auth.forms import UserCreationForm
+from django import forms
 
-from wow.models import Gamer
+from wow.models import Gamer, PlayableRace, PlayableClass
 
 
 class GamerCreationForm(UserCreationForm):
@@ -10,3 +11,27 @@ class GamerCreationForm(UserCreationForm):
             "playable_race",
             "playable_class"
         )
+
+
+# class GamerCreationForm(UserCreationForm):
+#     playable_race = forms.ModelChoiceField(
+#         queryset=PlayableRace.objects.all(),
+#         widget=forms.RadioSelect()
+#     )
+#     playable_class = forms.ModelChoiceField(
+#         queryset=PlayableClass.objects.all(),
+#         widget=forms.RadioSelect()
+#     )
+#
+#     class Meta(UserCreationForm.Meta):
+#         model = Gamer
+#         fields = UserCreationForm.Meta.fields + (
+#             "playable_race",
+#             "playable_class"
+#         )
+
+
+class GamerPlayableRaceClassUpdateForm(forms.ModelForm):
+    class Meta:
+        model = Gamer
+        fields = ["playable_race", "playable_class"]
