cde97a7e24f2c04e64a8aa89a901d04fe3e2a4d0
Tania Kharchuk <tkharchuk14@gmail.com>
2023-03-16 19:58:33 +0200

    Add ItemSearchForm to forms.py

--- wow/forms.py
+++ wow/forms.py
@@ -1,37 +1,43 @@
 from django.contrib.auth.forms import UserCreationForm
 from django import forms
 
-from wow.models import Gamer, PlayableRace, PlayableClass
+from wow.models import Gamer, PlayableRace, PlayableClass, ItemType
 
 
 class GamerCreationForm(UserCreationForm):
     class Meta(UserCreationForm.Meta):
         model = Gamer
-        fields = UserCreationForm.Meta.fields + (
-            "playable_race",
-            "playable_class"
-        )
-
-
-# class GamerCreationForm(UserCreationForm):
-#     playable_race = forms.ModelChoiceField(
-#         queryset=PlayableRace.objects.all(),
-#         widget=forms.RadioSelect()
-#     )
-#     playable_class = forms.ModelChoiceField(
-#         queryset=PlayableClass.objects.all(),
-#         widget=forms.RadioSelect()
-#     )
-#
-#     class Meta(UserCreationForm.Meta):
-#         model = Gamer
-#         fields = UserCreationForm.Meta.fields + (
-#             "playable_race",
-#             "playable_class"
-#         )
+        fields = UserCreationForm.Meta.fields
 
 
 class GamerPlayableRaceClassUpdateForm(forms.ModelForm):
     class Meta:
         model = Gamer
         fields = ["playable_race", "playable_class"]
+
+
+class ItemSearchForm(forms.Form):
+    name = forms.CharField(
+        max_length=255,
+        required=False,
+        label="",
+        widget=forms.TextInput(attrs={"placeholder": "Name"})
+    )
+    type = forms.ChoiceField(
+        choices=[("", "All")] + [(type.id, type.name) for type in ItemType.objects.all()],
+        required=False,
+        label="",
+    )
+    owner = forms.BooleanField(
+        required=False,
+        initial=False,
+        label="Show only my items"
+    )
+    created_at = forms.ChoiceField(
+        choices=(
+            ("-created_at", "From latest to oldest"),
+            ("created_at", "From oldest to latest"),
+        ),
+        required=False,
+        label=""
+    )
