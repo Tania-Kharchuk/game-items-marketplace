a680583bff682d46c928f21b1c350ae93c7b545e
Tania Kharchuk <tkharchuk14@gmail.com>
2023-03-16 11:10:50 +0200

    Add / to the end of urls in urls.py

--- wow/urls.py
+++ wow/urls.py
@@ -22,13 +22,13 @@ urlpatterns = [
     path("item_types/", ItemTypeListView.as_view(), name="item-type-list"),
     path("interaction_types/", InteractionTypeListView.as_view(), name="interaction-type-list"),
     path("items/", ItemListView.as_view(), name="item-list"),
-    path("items/create", ItemCreateView.as_view(), name="item-list"),
+    path("items/create/", ItemCreateView.as_view(), name="item-list"),
     path("items/<int:pk>/", ItemDetailView.as_view(), name="item-detail"),
     path("gamers/", GamerListView.as_view(), name="gamer-list"),
     path("gamers/<int:pk>/", GamerDetailView.as_view(), name="gamer-detail"),
     path("gamers/create/", GamerCreateView.as_view(), name="gamer-create"),
-    path("gamers/<int:pk>/update", GamerPlayableRaceClassUpdateView.as_view(), name="gamer-update"),
-    path("gamers/<int:pk>/delete", GamerDeleteView.as_view(), name="gamer-delete"),
+    path("gamers/<int:pk>/update/", GamerPlayableRaceClassUpdateView.as_view(), name="gamer-update"),
+    path("gamers/<int:pk>/delete/", GamerDeleteView.as_view(), name="gamer-delete"),
     ]
 
 app_name = "wow"
